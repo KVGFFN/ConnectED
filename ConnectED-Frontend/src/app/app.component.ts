f1247d4821b06e0b06363df2f3f833d24af0fca1
Kaine Van Geffen <kaine.vangeffen@gmail.com>
2024-02-26 20:42:42 +0100

    feat: import primeng

--- ConnectED-Frontend/src/app/app.component.ts
+++ ConnectED-Frontend/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
+import { Button, ButtonDirective, ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarModule, ButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'ConnectED-Frontend';
+  sidebarVisible = false;
 }
