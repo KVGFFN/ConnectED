e431c57a699f084b0f99597512479d6faae31a81
Kaine Van Geffen <kaine.vangeffen@gmail.com>
2024-03-10 19:08:09 +0100

    refactor: non-standalone project

--- ConnectED-Frontend/src/app/app.component.ts
+++ ConnectED-Frontend/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarModule } from 'primeng/sidebar';
-import { Button, ButtonDirective, ButtonModule } from 'primeng/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, SidebarModule, ButtonModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ConnectED-Frontend';
-  sidebarVisible = false;
 }
