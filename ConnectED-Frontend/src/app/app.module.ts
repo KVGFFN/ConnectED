078c1b4a6a1d3044c0d3624720111b4ad76570a1
Kaine Van Geffen <kaine.vangeffen@gmail.com>
2024-03-11 18:06:06 +0100

    feat: primeng

--- ConnectED-Frontend/src/app/app.module.ts
+++ ConnectED-Frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SidebarModule,
   ],
   providers: [
     provideClientHydration()
