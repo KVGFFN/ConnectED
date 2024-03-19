039a860e968cb106f89325d62b300011ff7055d3
Kaine Van Geffen <kaine.vangeffen@gmail.com>
2024-03-19 19:20:26 +0100

    temp: sidebar tweaks

--- ConnectED-Frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ ConnectED-Frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule],
+  imports: [SidebarModule, ButtonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
