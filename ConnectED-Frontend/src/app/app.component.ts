79031603b694c6dbccd420cb72132767da5d67bb
Kaine Van Geffen <kaine.vangeffen@frontforce.be>
2024-03-21 16:48:27 +0100

    feat: breadcrumb implementation

--- ConnectED-Frontend/src/app/app.component.ts
+++ ConnectED-Frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   sidebarVisible = false;
+
+  items: MenuItem[] = [];
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(event => {
+      this.items = this.buildBreadcrumbs((event as NavigationEnd).url)
+    })
+  }
+
+  buildBreadcrumbs(url: string): MenuItem[] {
+    const breadcrumbs: MenuItem[] = [];
+    const urlFragments = url.split('/');
+    urlFragments.forEach((fragment, index) => {
+      const label = fragment.charAt(0).toUpperCase() + fragment.slice(1);
+      const routerLink = index === 0 ? '/' : `/${urlFragments.slice(0, index + 1).join('/')}`;
+      breadcrumbs.push({ label, routerLink });
+    });
+    return breadcrumbs;
+  }
 }
