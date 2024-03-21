36726c7f3d471087b40f2c02369625fffbc25a1e
Kaine Van Geffen <kaine.vangeffen@frontforce.be>
2024-03-21 16:56:53 +0100

    refactor: styling

--- ConnectED-Frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ ConnectED-Frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBook, faHome } from '@fortawesome/free-solid-svg-icons';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [SidebarModule, ButtonModule, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  // icons
+  faHome = faHome;
+  faBook = faBook;
+
+  
   sidebarVisible = true;
 }
