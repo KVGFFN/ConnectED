bffa0fe96d4926b349349165b9e08a0bdeff3d93
Kaine Van Geffen <kaine.vangeffen@frontforce.be>
2024-03-21 16:56:25 +0100

    feat: import breadbar component

--- ConnectED-Frontend/src/app/app.module.ts
+++ ConnectED-Frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TabViewModule } from 'primeng/tabview';
 import { FormsModule } from '@angular/forms';
 import { PanelModule } from 'primeng/panel';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BreadcrumbBarComponent } from './shared/components/breadcrumb-bar/breadcrumb-bar.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserAnimationsModule,
     PanelModule,
     FontAwesomeModule,
+    BreadcrumbBarComponent
   ],
   providers: [
     provideClientHydration()
